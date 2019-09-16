51cae78989a9d95cd4868289263283509e920085
nwokiwu <51241053+nwokiwu@users.noreply.github.com>
2019-09-16 15:30:54 -0400

    updates

---
+++ client/src/Stateful Class components/Authenticated.js
@@ -0,0 +1,17 @@
+    import React from 'react';
+    import {Link} from 'react-router-dom';
+
+    export default ({context}) => {
+        const authUser = context.authenticatedUser.user;
+
+    return (
+        <div className="bounds" >
+            <div className = "grid-100" >
+            <h1 > {authUser.name
+            }, your account has been created! </h1> <p > Your username is {
+                authUser.username
+            } </p> <Link to = "/"
+            className = "button button-secondary"> Return to Courses </Link> \
+            </div> </div>
+        );
+    }
