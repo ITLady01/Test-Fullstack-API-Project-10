7ae5b9cdfb82593222cf6e62c5c34cefaa63ec4d
nwokiwu <51241053+nwokiwu@users.noreply.github.com>
2019-09-24 09:16:58 -0400

    Update UserSignup.js

--- client/src/components/UserSignup.js
+++ client/src/components/UserSignup.js
@@ -18,152 +18,152 @@ class UserSignUp extends Component {
         const name = event.target.name;
         const value = event.target.value;
 
-    //update the user password state
-    updateUserPassword = async (event) => {
-        await this.setState({
-            password: event.target.value
-        });
-        //check if current password state matches the password confirmation state
-        if (this.state.confirmationPassword === this.state.password) {
-            this.setState({
-                isConfirmed: true
-            }); //if true then password is confirmed
-        } else {
-            this.setState({
-                isConfirmed: false
-            }); //if false password is not confirmed
-        }
-    }
-
-    /* submit function that creates a new user and sends their credentials to the Express server */
-    submit = () => {
-        const { context } = this.props;
-        const {
-            firstName,
-            lastName,
-            emailAddress,
-            password,
-            confirmPassword
-        } = this.state;
-
-        let user = {};
-        /* check that all form fields are filled out, if not, display error message */
-        if (firstName === '' || lastName === '' || emailAddress === '' || password === '' || confirmPassword === '') {
-            this.setState({
-                errors: ['One or more fields are missing information, please check that all fields are filled out correctly']
-            })
-            return;
+        //update the user password state
+        updateUserPassword = async (event) => {
+            await this.setState({
+                password: event.target.value
+            });
+            //check if current password state matches the password confirmation state
+            if (this.state.confirmationPassword === this.state.password) {
+                this.setState({
+                    isConfirmed: true
+                }); //if true then password is confirmed
+            } else {
+                this.setState({
+                    isConfirmed: false
+                }); //if false password is not confirmed
+            }
         }
 
-        if (password !== confirmPassword) {     // display error message if passwords don't match
-            this.setState({
-                errors: ['The entered passwords do not match']
-            })
-            return;
-
-        } else {        // otherwise set properties for user
-            user = {
+        /* submit function that creates a new user and sends their credentials to the Express server */
+        submit = () => {
+            const { context } = this.props;
+            const {
                 firstName,
                 lastName,
                 emailAddress,
-                password
-            };
+                password,
+                confirmPassword
+            } = this.state;
+
+            let user = {};
+            /* check that all form fields are filled out, if not, display error message */
+            if (firstName === '' || lastName === '' || emailAddress === '' || password === '' || confirmPassword === '') {
+                this.setState({
+                    errors: ['One or more fields are missing information, please check that all fields are filled out correctly']
+                })
+                return;
+            }
+
+            if (password !== confirmPassword) {     // display error message if passwords don't match
+                this.setState({
+                    errors: ['The entered passwords do not match']
+                })
+                return;
+
+            } else {        // otherwise set properties for user
+                user = {
+                    firstName,
+                    lastName,
+                    emailAddress,
+                    password
+                };
+            }
+
+            /* returns a promise: either an array of errors (sent from the API if the response is 400), or an empty array (if the response is 201) */
+            context.data.createUser(user)
+                .then(res => {
+                    if (res.status === 500) {
+                        this.props.history.push('/error');    // redirects url to error route
+                    } else {
+                        //if the user is signed up successfully, automatically sign them in to the site
+                        context.actions.signIn(emailAddress, password)
+                            .then(() => {
+                                this.props.history.push('/');
+                            });
+                    }
+                })
+                .catch((err) => {        // handles a rejected promise returned by createUser()
+                    console.log(err);
+                    this.setState({
+                        errors: ['That email is already being used.']
+                    })
+                    // this.props.history.push('/error');    // redirects url to error route
+                });
         }
 
-        /* returns a promise: either an array of errors (sent from the API if the response is 400), or an empty array (if the response is 201) */
-        context.data.createUser(user)
-            .then(res => {
-                if (res.status === 500) {
-                    this.props.history.push('/error');    // redirects url to error route
-                } else {
-                    //if the user is signed up successfully, automatically sign them in to the site
-                    context.actions.signIn(emailAddress, password)
-                        .then(() => {
-                            this.props.history.push('/');
-                        });
-                }
-            })
-            .catch((err) => {        // handles a rejected promise returned by createUser()
-                console.log(err);
-                this.setState({
-                    errors: ['That email is already being used.']
-                })
-                // this.props.history.push('/error');    // redirects url to error route
-            });
-    }
+        /* cancel function */
+        cancel = () => {
+            this.props.history.push('/');    // redirects to main page of app
+        }
 
-    /* cancel function */
-    cancel = () => {
-        this.props.history.push('/');    // redirects to main page of app
-    }
+        render() {
+            const {
+                firstName,
+                lastName,
+                emailAddress,
+                password,
+                confirmPassword,
+                errors
+            } = this.state;
 
-    render() {
-        const {
-            firstName,
-            lastName,
-            emailAddress,
-            password,
-            confirmPassword,
-            errors
-        } = this.state;
-
-        /* returns input fields to be used in each form */
-        return (
-            <div className='bounds'>
-                <div className='grid-33 centered signin'>
-                    <h1>Sign Up</h1>
-                    <Form
-                        cancel={this.cancel}
-                        errors={errors}
-                        submit={this.submit}
-                        submitButtonText='Sign Up'
-                        elements={() => (
-                            <React.Fragment>
-                                <input
-                                    id='firstName'
-                                    name='firstName'
-                                    type='text'
-                                    className=''
-                                    value={firstName}
-                                    onChange={this.update}
-                                    placeholder='First Name' />
-                                <input
-                                    id='lastName'
-                                    name='lastName'
-                                    type='text'
-                                    className=''
-                                    value={lastName}
-                                    onChange={this.update}
-                                    placeholder='Last Name' />
-                                <input
-                                    id='emailAddress'
-                                    name='emailAddress'
-                                    type='text'
-                                    className=''
-                                    value={emailAddress}
-                                    onChange={this.update}
-                                    placeholder='Email Address' />
-                                <input id='password'
-                                    name='password'
-                                    type='password'
-                                    className=''
-                                    value={password}
-                                    onChange={this.update}
-                                    placeholder='Password' />
-                                <input id='confirmPassword'
-                                    name='confirmPassword'
-                                    type='password'
-                                    className=''
-                                    value={confirmPassword}
-                                    onChange={this.update}
-                                    placeholder='Confirm Password' />
-                            </React.Fragment>
-                        )} />
-                    <p> Already have a user account? <Link to='/signin'>Click here</Link> to sign in! </p>
+            /* returns input fields to be used in each form */
+            return (
+                <div className='bounds'>
+                    <div className='grid-33 centered signin'>
+                        <h1>Sign Up</h1>
+                        <Form
+                            cancel={this.cancel}
+                            errors={errors}
+                            submit={this.submit}
+                            submitButtonText='Sign Up'
+                            elements={() => (
+                                <React.Fragment>
+                                    <input
+                                        id='firstName'
+                                        name='firstName'
+                                        type='text'
+                                        className=''
+                                        value={firstName}
+                                        onChange={this.update}
+                                        placeholder='First Name' />
+                                    <input
+                                        id='lastName'
+                                        name='lastName'
+                                        type='text'
+                                        className=''
+                                        value={lastName}
+                                        onChange={this.update}
+                                        placeholder='Last Name' />
+                                    <input
+                                        id='emailAddress'
+                                        name='emailAddress'
+                                        type='text'
+                                        className=''
+                                        value={emailAddress}
+                                        onChange={this.update}
+                                        placeholder='Email Address' />
+                                    <input id='password'
+                                        name='password'
+                                        type='password'
+                                        className=''
+                                        value={password}
+                                        onChange={this.update}
+                                        placeholder='Password' />
+                                    <input id='confirmPassword'
+                                        name='confirmPassword'
+                                        type='password'
+                                        className=''
+                                        value={confirmPassword}
+                                        onChange={this.update}
+                                        placeholder='Confirm Password' />
+                                </React.Fragment>
+                            )} />
+                        <p> Already have a user account? <Link to='/signin'>Click here</Link> to sign in! </p>
+                    </div>
                 </div>
-            </div>
-        );
+            );
+        }
     }
-}
 
 export default UserSignUp;
